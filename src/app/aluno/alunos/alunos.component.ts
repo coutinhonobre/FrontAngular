a42c5d9c5fb0660e17d387d350df37d221ee2443
coutinhoigor <81998304+coutinhoigor@users.noreply.github.com>
2021-08-16 03:09:15 -0300

    addicionado na lista de alunos descricoes

--- src/app/aluno/alunos/alunos.component.ts
+++ src/app/aluno/alunos/alunos.component.ts
@@ -16,7 +16,7 @@ export class AlunosComponent implements OnInit {
   aluno: Aluno = new Aluno();
 
   alunoDataSource: MatTableDataSource<Aluno>;
-  displayedAlunos: String[] = ['idaluno', 'nome', 'update', 'delete'];
+  displayedAlunos: String[] = ['idaluno', 'nome', 'sexo', 'dt_nasc', 'update', 'delete'];
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
