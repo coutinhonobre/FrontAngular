9305d4588b064a6cd5e1845695d0c513f516daa0
larissamato <matoslarissa31@gmail.com>
2023-11-15 20:40:49 -0300

    :fire: #62 removed unnecessary imports

--- src/app/professores/professor.service.ts
+++ src/app/professores/professor.service.ts
@@ -1,7 +1,4 @@
 import { Injectable } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { HttpClient } from '@angular/common/http';
-import { Observable} from 'rxjs';
 import { BackendApiService } from '../backend-api.service';
 
 @Injectable({
